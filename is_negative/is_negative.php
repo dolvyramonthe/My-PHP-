c8c8940e8051d82e663c40f5dc341f2af13ab403
roseline <monthe_r@etna-alternance.net>
2023-11-08 12:33:47 +0100

    Créez une fonction qui prend en paramètre un nombre et affiche :is_negative

---
+++ is_negative/is_negative.php
@@ -0,0 +1,14 @@
+<?php
+function is_negative($number) {
+    if (is_numeric($number)) {
+        if ($number < 0) {
+            echo "True\n";
+        } else {
+            echo "False\n";
+        }
+    } else {
+        echo "Le paramètre n'est pas un Int\n";
+    }
+}
+?>
+
